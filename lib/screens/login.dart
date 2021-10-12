46fcce9de218d8ff4bdcd51219699babc7f0cb99
santiagodr <santiagodromerosw@hotmail.com>
2021-10-12 13:03:59 -0300

    Login page

---
+++ lib/screens/login.dart
@@ -0,0 +1,255 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  int activeIndex = 0;
+
+  @override
+  void initState() {
+    Timer.periodic(Duration(seconds: 5), (timer) {
+      setState(() {
+        activeIndex++;
+
+        if (activeIndex == 4) activeIndex = 0;
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Container(
+                child: Image.network(
+              'https://i.imgur.com/EXUF2Fl.png',
+              height: 60,
+            )),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 350,
+              child: Stack(children: [
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: AnimatedOpacity(
+                    opacity: activeIndex == 0 ? 1 : 0,
+                    duration: Duration(
+                      seconds: 1,
+                    ),
+                    curve: Curves.linear,
+                    child: Image.network(
+                      'https://i.imgur.com/K37Y9HS.png',
+                      height: 400,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: AnimatedOpacity(
+                    opacity: activeIndex == 1 ? 1 : 0,
+                    duration: Duration(seconds: 1),
+                    curve: Curves.linear,
+                    child: Image.network(
+                      'https://i.imgur.com/78CvihZ.png',
+                      height: 250,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: AnimatedOpacity(
+                    opacity: activeIndex == 2 ? 1 : 0,
+                    duration: Duration(seconds: 1),
+                    curve: Curves.linear,
+                    child: Image.network(
+                      'https://i.imgur.com/9mlOKe2.png',
+                      height: 250,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  child: AnimatedOpacity(
+                    opacity: activeIndex == 3 ? 1 : 0,
+                    duration: Duration(seconds: 1),
+                    curve: Curves.linear,
+                    child: Image.network(
+                      'https://i.imgur.com/rUq1PPb.png',
+                      height: 250,
+                    ),
+                  ),
+                )
+              ]),
+            ),
+            Text(
+              "RF Builder",
+              style: TextStyle(
+                  fontSize: 36,
+                  fontFamily: "Titillium Web",
+                  fontWeight: FontWeight.w600,
+                  color: Colors.grey.shade700),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextField(
+              cursorColor: Colors.black,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(0.0),
+                labelText: 'Email',
+                hintText: 'Username or e-mail',
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w400,
+                ),
+                hintStyle: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 14.0,
+                ),
+                prefixIcon: Icon(
+                  Iconsax.user,
+                  color: Colors.red,
+                  size: 18,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                floatingLabelStyle: TextStyle(
+                  color: Colors.red,
+                  fontSize: 18.0,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 1.5),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              cursorColor: Colors.red,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(0.0),
+                labelText: 'Password',
+                hintText: 'Password',
+                hintStyle: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 14.0,
+                ),
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w400,
+                ),
+                prefixIcon: Icon(
+                  Iconsax.key,
+                  color: Colors.red,
+                  size: 18,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                floatingLabelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 1.5),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w400),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            MaterialButton(
+              onPressed: () {},
+              height: 45,
+              color: Colors.red,
+              child: Text(
+                "Login",
+                style: TextStyle(color: Colors.white, fontSize: 16.0),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Don\'t have an account?',
+                  style: TextStyle(
+                      color: Colors.grey.shade600,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w400),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w400),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
